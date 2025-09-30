72c8e1102e48a66688acccdacdf2a34a12f2848f
Silvia Reis <silviareis104@gmail.com>
2025-09-30 15:27:11 -0300

    primeiro teste

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// let a = 10;
+// let b = 20;
+
+// if (a > b) {
+//    console.log("O maior é a:", a);
+// } else if (b > a) {
+//    console.log("O maior é b:", b);
+// } else {
+//    console.log("Os valores são iguais.");
+// }
+// function verificarMaior() {
+//       const n1 = parseFloat(document.getElementById('num1').value);
+//       const n2 = parseFloat(document.getElementById('num2').value);
+//       const maior = document.getElementById('maior');
+//       if (isNaN(n1) || isNaN(n2)) {
+//          maior.value = 'Insira ambos os números';
+//       } else {
+//          maior.value = n1 > n2 ? n1 : n2;
+//       }
+//    }
+
